129820fe89ac61a231f4f8644f7e3c21f5f7e7ed
riswandaP <riswandaputra03@gmail.com>
2026-02-24 21:06:32 +0700

    Konversi Rumus Luas Persegi Panjang

---
+++ file2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main() { //mulai
+    int p, l, luas; //deklarasi
+
+    //langkah 2
+    cout << "Masukkan panjang =";
+    cin >> p;
+
+    //langkah 3
+    cout << "Masukkan lebar =";
+    cin >> l;
+
+    //langkah 4
+    luas = p * l;
+
+    //langkah 5
+    cout << "Hasilnya =" << luas << endl;
+    cout << "terima kasih";
+} //selesai
